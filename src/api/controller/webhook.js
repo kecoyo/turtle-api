f3bcff46ff5f3eec4a94c33d322b68ea69f3ba94
杨可可 <kecoyo@163.com>
2021-03-18 23:34:54 +0800

    修改apidoc为doc； 修改pm2.json监听和重启；

--- src/api/controller/webhook.js
+++ src/api/controller/webhook.js
@@ -1,39 +1,35 @@
-const _ = require('lodash');
-
 /**
  * WebHook
  */
 module.exports = class extends think.Controller {
   /**
-   * @api {get} /api/webhook/push   1.git push webhook
-   * @apiGroup 图标分类
+   * @api {post} /api/webhook/push   1.git push webhook
+   * @apiGroup WebHook
    *
-   * @apiSuccess {Object[]} data  图标分类列表
+   * @apiSuccess {Object[]} data  stdout
    *
-   * @apiSuccessExample {json} Success-Response:
-   *     HTTP/1.1 200 OK
-   *     [
-   *       {
-   *         "id": 1,           // 分类ID
-   *         "name": "应用类",   // 分类名称
-   *         "icons": [         // 分类下图标列表
-   *           "//cdn.kecoyo.com/upload/butler_icon/c8/de7389a98521305778402ba8afec13.png",
-   *           "//cdn.kecoyo.com/upload/butler_icon/c8/de7389a98521305778402ba8afec13.png",
-   *           ...
-   *         ],
-   *       },
-   *       ...
-   *     ]
    */
   async pushAction() {
-    try {
-      var cmd = 'git pull';
-      const result = await think.exec(cmd);
-      think.logger.info('exec command: ' + cmd + '\n\n' + result);
-      return this.success(result);
-    } catch (e) {
-      think.logger.error(e);
-      return this.fail(e.message);
+    const headers = this.ctx.header;
+    // const body = this.post();
+
+    // think.logger.debug('headers:', headers);
+    // think.logger.debug('body:', body);
+
+    if (this.isPost && headers['x-gitee-token'] === 'kecoyo' && headers['x-gitee-event'] === 'Push Hook') {
+      try {
+        var cmd = 'git pull';
+        const result = await think.exec(cmd);
+        think.logger.info('exec command: ' + cmd + '\n\n' + result);
+        return this.success(result);
+      } catch (e) {
+        think.logger.error(e);
+        return this.fail(e.message);
+      }
     }
+
+    think.logger.warn('请求失败！');
+    think.logger.warn(JSON.stringify(headers));
+    return this.fail('请求失败！', headers);
   }
 };
