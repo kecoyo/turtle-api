691381c4b87b727ff2fd345a2986bc2196def520
杨可可 <kecoyo@163.com>
2022-04-26 16:51:06 +0800

    优化框架；

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -4,4 +4,9 @@ module.exports = class extends Base {
   indexAction() {
     return this.display();
   }
+
+  listFilesAction() {
+    const files = think.getdirFiles('./src');
+    return this.success(files);
+  }
 };
